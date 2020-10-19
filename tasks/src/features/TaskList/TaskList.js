1a2633d07f719bb1521bc064f6530bf9b221e416
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-19 12:24:15 -0300

    feat: Create initial screen

---
+++ tasks/src/features/TaskList/TaskList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { TextStyle } from "./styles";
+
+import imgToday from "../../../assets/imgs/today.jpg";
+
+const TaskList = () => {
+    return (
+        <TextStyle>
+            Hello World
+        </TextStyle>
+    );
+};
+
+export default TaskList;
