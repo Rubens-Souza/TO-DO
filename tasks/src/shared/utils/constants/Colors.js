cf71d27556f38b3dcf605bd4d3a29234181161f5
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-21 13:40:45 -0300

    feat(colors): Add more color to the palette

--- tasks/src/shared/utils/constants/Colors.js
+++ tasks/src/shared/utils/constants/Colors.js
@@ -1,9 +1,11 @@
 const Colors = {
     white: "#ffffff",
     lightBlack: "#636363",
+    black: "#1d1d1d",
     checkboxGreen: "#61c942",
     lightGray: "#cccccc",
     gray: "#8c8c8c",
+    darkRed: "#AD4D46",
 };
 
 export default Colors;
