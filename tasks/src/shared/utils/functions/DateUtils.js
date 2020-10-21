d7817b0627f160d8cc4f39cbc0c255ba22ac5922
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-21 11:36:03 -0300

    feat(utils): Add method to getActualDate

--- tasks/src/shared/utils/functions/DateUtils.js
+++ tasks/src/shared/utils/functions/DateUtils.js
@@ -4,6 +4,10 @@ import "moment/locale/en-ca";
 export const DateLocale = "en-ca";
 export const DateFormat = "ddd, MM/D/YYYY";
 
+export const getActualDate = () => {
+    return new Date();
+};
+
 export const getActualFormattedDate = () => {
     return moment().locale(DateLocale).format(DateFormat);
 };
