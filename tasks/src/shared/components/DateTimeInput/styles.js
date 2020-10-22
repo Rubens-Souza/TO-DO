0ac7cd0cccb9d55e1537d5ad7b101b9a1b37cbdb
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-10-22 12:58:22 -0300

    fix(components): Change border of DatePickerInput

--- tasks/src/shared/components/DateTimeInput/styles.js
+++ tasks/src/shared/components/DateTimeInput/styles.js
@@ -1,5 +1,11 @@
 import styled from "styled-components/native";
-
 import { StyledCommumTextInput } from "../../CommumStyles/CommumStyles";
 
-export const DateTimeFakeInput = styled(StyledCommumTextInput)``;
+import Colors from "../../../shared/utils/constants/Colors";
+
+export const DateTimeFakeInput = styled(StyledCommumTextInput)`
+    border: none;
+    border-bottom-color: ${Colors.lightGray};
+    border-bottom-width: 1px;
+    font-size: 15px;
+`;
